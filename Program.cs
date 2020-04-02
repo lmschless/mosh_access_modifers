2b7cc455bf9f9be37f0b0f65013f645a2b267c99
Luke <lmschless@gmail.com>
2020-04-01 22:22:18 -0700

    working birthdate methods for access modifier practice.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,31 @@
 
 namespace AccessModifiers
 {
+    public class Person
+    {
+        // when variable(field) is private, use an UNDERSCORE and CAMELCASE.
+        private DateTime _birthdate;
+
+        public void SetBirthDate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        // Use type DateTime because you are passing data.
+        public DateTime GetBirthDate()
+        {
+            return _birthdate;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = new Person();
+            person.SetBirthDate(new DateTime(1989, 4, 11));
+            Console.WriteLine(person.GetBirthDate());
         }
     }
 }
