5c1949e74a296f9f4e794bd5fdb30b027c70bae9
Luke <lmschless@gmail.com>
2020-04-02 16:48:55 -0700

    working on different types of getters and setters.

--- Program.cs
+++ Program.cs
@@ -4,13 +4,31 @@ namespace AccessModifiers
 {
     public class Person
     {
+        // complier automatically creates a private field when it sees get; set;
+        public DateTime Birthdate { get; set; }
+    }
+
+
+    // old way of using private field, getter, and setter.
+    public class Person
+    {
         // when variable(field) is private, use an UNDERSCORE and CAMELCASE.
         private DateTime _birthdate;
 
+        // using explicit properties to get and set a private field
+        public DateTime Birthdate
+        {
+            get { return _birthdate; }
+            set { _birthdate = value; }
+        }
+
+        // using the manual way to get and set a private field
         public void SetBirthDate(DateTime birthdate)
         {
             _birthdate = birthdate;
         }
+
+
 
         // Use type DateTime because you are passing data.
         public DateTime GetBirthDate()
