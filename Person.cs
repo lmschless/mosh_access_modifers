ff9e1e125bb2e57e405166167663f6eedf94b7b1
Luke <lmschless@gmail.com>
2020-04-02 17:15:14 -0700

    * Person.cs: final commit in this practice section
    * Program.cs:

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        // create a constructor that sets the birthdate once and it cannot be changed.
+        public string Name { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        // complier automatically creates a private field when it sees get; set;
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,50 +1,51 @@
 ﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace AccessModifiers
 {
-    public class Person
-    {
-        // complier automatically creates a private field when it sees get; set;
-        public DateTime Birthdate { get; set; }
-    }
 
 
     // old way of using private field, getter, and setter.
-    public class Person
-    {
-        // when variable(field) is private, use an UNDERSCORE and CAMELCASE.
-        private DateTime _birthdate;
+    //public class Person
+    //{
+    //    // when variable(field) is private, use an UNDERSCORE and CAMELCASE.
+    //    private DateTime _birthdate;
 
-        // using explicit properties to get and set a private field
-        public DateTime Birthdate
-        {
-            get { return _birthdate; }
-            set { _birthdate = value; }
-        }
+    // using explicit properties to get and set a private field
+    //    public DateTime Birthdate
+    //    {
+    //        get { return _birthdate; }
+    //        set { _birthdate = value; }
+    //    }
 
-        // using the manual way to get and set a private field
-        public void SetBirthDate(DateTime birthdate)
-        {
-            _birthdate = birthdate;
-        }
+    //    // using the manual way to get and set a private field
+    //    public void SetBirthDate(DateTime birthdate)
+    //    {
+    //        _birthdate = birthdate;
+    //    }
 
 
 
-        // Use type DateTime because you are passing data.
-        public DateTime GetBirthDate()
-        {
-            return _birthdate;
-        }
-    }
+    //    // Use type DateTime because you are passing data.
+    //    public DateTime GetBirthDate()
+    //    {
+    //        return _birthdate;
+    //    }
+    //}
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.SetBirthDate(new DateTime(1989, 4, 11));
-            Console.WriteLine(person.GetBirthDate());
+            // old way with explicit getters and setters
+            //var person = new Person();
+            //person.SetBirthDate(new DateTime(1989, 4, 11));
+            //Console.WriteLine(person.GetBirthDate());
+
+            // new way
+            var person = new Person(new DateTime(1989, 4, 11));
+            Console.WriteLine(person.Age);
         }
     }
 }
